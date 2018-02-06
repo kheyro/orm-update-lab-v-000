9de2634a9f2bf2fb97f44c17d761d657a57cf13b
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 15:01:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,11 +20,21 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
-
+  
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS students")
   end
 
+  def create 
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
   def save
     if self.id
       update
