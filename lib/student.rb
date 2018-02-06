331a12fc3ccccc2a017c45f405aedf71616ee3c6
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 15:00:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-  def find_by_name(name)
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students WHERE name = ?;
     SQL
